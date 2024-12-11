4545d4a3d73b24c38ab6a6bd851506e19ed1c969
Sunjit Tara <sunjit@gmail.com>
2024-12-10 22:08:24 -0600

    Added assumptions

---
+++ HealthyCoderApp/src/test/java/com/healthycoderapp/BMICalculatorSkippedTest.java
@@ -0,0 +1,28 @@
+package com.healthycoderapp;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+// This shows how to skip a test
+public class BMICalculatorSkippedTest {
+
+    private String environment = "dev";
+
+    @Test
+    void should_ReturnTrue_When_DietRecommended() {
+
+	// Note: It is assumeTrue, NOT assertTrue
+	assumeTrue(this.environment.equals("prod"));
+	// arrange
+	double weight = 89.0;
+	double height = 1.72;
+	
+	// act
+	boolean recommended = BMICalculator.isDietRecommended(weight, height);
+	
+	// assert
+	assertTrue(recommended);
+    }
+}
